4f89127d018ee42ad11348b3a8413ae222d6ad0b
mnovana <maravicnovana@gmail.com>
2024-09-02 14:28:54 +0200

    Add FoodController unit tests

--- service/KitchenDisplaySystem/KitchenDisplaySystem.Tests/Controllers/FoodControllerTests.cs
+++ service/KitchenDisplaySystem/KitchenDisplaySystem.Tests/Controllers/FoodControllerTests.cs
@@ -53,5 +53,276 @@ namespace KitchenDisplaySystem.Tests.Controllers
                 Assert.Equal(food[i].FoodType.Name, listResult[i].FoodTypeName);
             }
         }
+
+        [Fact]
+        public async Task GetFoodById_ValidId_ReturnsObject()
+        {
+            // Arrange
+            Food food = new Food { 
+                Id = 1, 
+                Name = "Brusketi sa paradajzom i bosiljkom", 
+                FoodTypeId = 1, 
+                FoodType = new FoodType { Id = 1, Name = "Predjelo" } 
+            };
+
+            var mockRepository = new Mock<IFoodRepository>();
+            mockRepository.Setup(x => x.GetByIdAsync(1)).ReturnsAsync(food);
+
+            var mapperConfiguration = new MapperConfiguration(cfg => cfg.AddProfile(new MappingProfiles()));
+            var mapper = mapperConfiguration.CreateMapper();
+
+            var foodController = new FoodController(mockRepository.Object, mapper);
+
+            // Act
+            var result = await foodController.GetFoodById(1) as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotNull(result.Value);
+
+            var objectResult = (FoodDTO)result.Value;
+
+            Assert.Equal(food.Id, objectResult.Id);
+            Assert.Equal(food.Name, objectResult.Name);
+            Assert.Equal(food.FoodTypeId, objectResult.FoodTypeId);
+            Assert.Equal(food.FoodType.Name, objectResult.FoodTypeName);
+        }
+
+        [Fact]
+        public async Task GetFoodById_InvalidId_ReturnsNotFound()
+        {
+            // Assert
+            var mockRepository = new Mock<IFoodRepository>();
+
+            var mockMapper = new Mock<IMapper>();
+
+            var foodController = new FoodController(mockRepository.Object, mockMapper.Object);
+
+            // Act
+            var result = await foodController.GetFoodById(1) as NotFoundResult;
+            
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task DeleteFood_ValidId_ReturnsNoContent()
+        {
+            // Assert
+            var mockRepository = new Mock<IFoodRepository>();
+            mockRepository.Setup(x => x.DeleteAsync(1)).ReturnsAsync(true);
+
+            var mockMapper = new Mock<IMapper>();
+
+            var foodController = new FoodController(mockRepository.Object, mockMapper.Object);
+
+            // Act
+            var result = await foodController.DeleteFood(1) as NoContentResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task DeleteFood_InvalidId_ReturnsBadRequest()
+        {
+            // Assert
+            var mockRepository = new Mock<IFoodRepository>();
+            mockRepository.Setup(x => x.DeleteAsync(1)).ReturnsAsync(false);
+
+            var mockMapper = new Mock<IMapper>();
+
+            var foodController = new FoodController(mockRepository.Object, mockMapper.Object);
+
+            // Act
+            var result = await foodController.DeleteFood(1) as BadRequestResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task PostFood_ValidModelState_ReturnsCreatedAtAction()
+        {
+            // Arrange
+            Food userFood = new Food()
+            {
+                Name = "Brusketi sa paradajzom i bosiljkom",
+                FoodTypeId = 1
+            };
+
+            Food dbFood = new Food()
+            {
+                Id = 1,
+                Name = "Brusketi sa paradajzom i bosiljkom",
+                Deleted = false,
+                FoodTypeId = 1,
+                FoodType = new FoodType { Id = 1, Name = "Predjelo" }
+            };
+
+            var mockRepository = new Mock<IFoodRepository>();
+            mockRepository.Setup(x => x.AddAsync(userFood)).Callback(() => userFood.Id = 1);
+            mockRepository.Setup(x => x.GetByIdAsync(1)).ReturnsAsync(dbFood);
+
+            var mapperConfiguration = new MapperConfiguration(cfg => cfg.AddProfile(new MappingProfiles()));
+            var mapper = mapperConfiguration.CreateMapper();
+
+            var foodController = new FoodController(mockRepository.Object, mapper);
+
+            // Act
+            var result = await foodController.PostFood(userFood) as CreatedAtActionResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotNull(result.Value);
+            Assert.Equal("GetFoodById", result.ActionName);
+            Assert.Equal(1, result.RouteValues["id"]);
+
+            var objectResult = (FoodDTO)result.Value;
+
+            Assert.Equal(dbFood.Name, objectResult.Name);
+            Assert.Equal(dbFood.FoodTypeId, objectResult.FoodTypeId);
+            Assert.Equal(dbFood.FoodType.Name, objectResult.FoodTypeName);
+        }
+
+        [Fact]
+        public async Task PostFood_InvalidModelState_ReturnsBadRequest()
+        {
+            // Arrange
+            var food = new Food();
+            var mockRepository = new Mock<IFoodRepository>();
+            var mockMapper = new Mock<IMapper>();
+
+            var foodController = new FoodController(mockRepository.Object, mockMapper.Object);
+
+            foodController.ModelState.AddModelError("test", "test");
+
+            // Act
+            var result = await foodController.PostFood(food) as BadRequestResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task PostFood_AddAsyncError_ReturnsBadRequest()
+        {
+            // Arrange
+            var food = new Food();
+
+            var mockRepository = new Mock<IFoodRepository>();
+            mockRepository.Setup(x => x.AddAsync(food)).ThrowsAsync(new Exception());
+
+            var mockMapper = new Mock<IMapper>();
+
+            var foodController = new FoodController(mockRepository.Object, mockMapper.Object);
+
+            // Act
+            var result = await foodController.PostFood(food) as BadRequestResult;
+        }
+
+        [Fact]
+        public async Task PutFood_ValidRequest_ReturnsOkObject()
+        {
+            // Arrange
+            Food userFood = new Food()
+            {
+                Id = 1,
+                Name = "Brusketi sa paradajzom i bosiljkom",
+                FoodTypeId = 1
+            };
+
+            Food dbFood = new Food()
+            {
+                Id = 1,
+                Name = "Brusketi sa paradajzom i bosiljkom",
+                Deleted = false,
+                FoodTypeId = 1,
+                FoodType = new FoodType { Id = 1, Name = "Predjelo" }
+            };
+
+            var mockRepository = new Mock<IFoodRepository>();
+            mockRepository.Setup(x => x.GetByIdAsync(1)).ReturnsAsync(dbFood);
+
+            var mapperConfiguration = new MapperConfiguration(cfg => cfg.AddProfile(new MappingProfiles()));
+            var mapper = mapperConfiguration.CreateMapper();
+
+            var foodController = new FoodController(mockRepository.Object, mapper);
+
+            // Act
+            var result = await foodController.PutFood(1, userFood) as OkObjectResult;
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.NotNull(result.Value);
+
+            var objectResult = (FoodDTO)result.Value;
+
+            Assert.Equal(dbFood.Id, objectResult.Id);
+            Assert.Equal(dbFood.Name, objectResult.Name);
+            Assert.Equal(dbFood.FoodTypeId, objectResult.FoodTypeId);
+            Assert.Equal(dbFood.FoodType.Name, objectResult.FoodTypeName);
+        }
+
+        [Fact]
+        public async Task PutFood_InvalidId_ReturnsBadRequest()
+        {
+            // Arrange
+            Food food = new Food()
+            {
+                Id = 1,
+                Name = "test",
+                FoodTypeId = 1
+            };
+
+            var mockRepository = new Mock<IFoodRepository>();
+            var mockMapper = new Mock<IMapper>();
+
+            var foodController = new FoodController(mockRepository.Object, mockMapper.Object);
+
+            // Act
+            var result = await foodController.PutFood(2, food) as BadRequestResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task PutFood_InvalidModelState_ReturnsBadRequest()
+        {
+            // Arrange
+            var food = new Food();
+            var mockRepository = new Mock<IFoodRepository>();
+            var mockMapper = new Mock<IMapper>();
+
+            var foodController = new FoodController(mockRepository.Object, mockMapper.Object);
+            foodController.ModelState.AddModelError("test", "test");
+
+            // Act
+            var result = await foodController.PutFood(1, food) as BadRequestResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task PutFood_UpdateAsyncError_ReturnsBadRequest()
+        {
+            // Arrange
+            var food = new Food() { Id = 1 };
+
+            var mockRepository = new Mock<IFoodRepository>();
+            mockRepository.Setup(x => x.UpdateAsync(food)).ThrowsAsync(new Exception());
+
+            var mockMapper = new Mock<IMapper>();
+
+            var foodController = new FoodController(mockRepository.Object, mockMapper.Object);
+
+            // Act
+            var result = await foodController.PutFood(1, food) as BadRequestResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
     }
 }
